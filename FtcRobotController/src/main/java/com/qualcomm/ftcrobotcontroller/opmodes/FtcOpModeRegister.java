abe427a4a3057a7247d1909362297b7e35cb9c0f
preescoot <prestomahon@gmail.com>
2015-11-18 17:32:36 -0700

    preescoot

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -44,5 +44,9 @@ public class FtcOpModeRegister implements OpModeRegister {
     manager.register("LinearOP", AutonomousREDTEAM.class);
     manager.register("TeleOP", TeleOP.class);
     manager.register("Sensor Test", TestSensors.class);
+    manager.register("Autonomous BLUE TEAM", AutonomousBLUETEAM.class);
+    manager.register("Autonomous RED TEAM", AutonomousREDTEAM.class);
+    manager.register("Autonomous Backup BLUE TEAM", AutonomousBackUpBLUETEAM.class);
+    manager.register("Autonomous Backup RED TEAM", AutonomousBackUpREDTEAM.class);
   }
 }
