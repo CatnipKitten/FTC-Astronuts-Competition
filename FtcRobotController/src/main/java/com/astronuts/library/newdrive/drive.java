5f1e04381d3c719d4eeaec7349fcfc9837a3a972
CatnipKitten <kerbonauts@gmail.com>
2015-11-27 14:44:14 -0700

    Added new drive software, and deprecated the old drive software

---
+++ FtcRobotController/src/main/java/com/astronuts/library/newdrive/drive.java
@@ -0,0 +1,62 @@
+package com.astronuts.library.newdrive;
+
+import com.astronuts.library.RobotData;
+import com.astronuts.library.movement.EncoderMotor;
+
+public class drive {
+    private EncoderMotor encoderLeft;
+    private EncoderMotor encoderRight;
+
+    public drive(EncoderMotor encoderLeft, EncoderMotor encoderRight) {
+        this.encoderLeft = encoderLeft;
+        this.encoderRight = encoderRight;
+    }
+
+    //Default drive in centimeters
+    public void driveByDistance(double distance){ driveByDistance(distance, 'c'); }
+    public void driveByDistance(double distance, char unit){
+        switch(unit){
+            case'i':
+                distance *= 2.54;
+                break;
+            default:
+                break;
+        }
+        encoderLeft.move(distanceToEncoderTicks(distance), 0.5);
+        encoderRight.move(distanceToEncoderTicks(distance), 0.5);
+    }
+
+    //Defaults to turning 180 degrees
+    public void turnByAngle(double amt, char dir){ turnByAngle(amt, dir, 'd'); }
+    public void turnByAngle(double amt, char dir, char unit){
+        double var = amt; //Acts as a holder for the data
+        switch(unit){
+            case'r':
+                var = Math.toDegrees(amt);
+                break;
+            case'd':
+            default: var = amt;
+                break;
+        }
+        if(dir == 'l') {
+            encoderLeft.move(-degreesToEncoderTicks(var), 0.5);
+            encoderRight.move(degreesToEncoderTicks(var), 0.5);
+        }
+        else{
+            encoderLeft.move(degreesToEncoderTicks(var), 0.5);
+            encoderRight.move(-degreesToEncoderTicks(var), 0.5);
+        }
+    }
+    private int distanceToEncoderTicks(double amt){
+        //TODO: calculate how far one revolution is in cm
+        double t;
+        t = (RobotData.encoderTicksPerRevolution * amt) / RobotData.distancePerRevolution;
+        return (int) Math.round(t);
+    }
+    private int degreesToEncoderTicks(double amt){
+        //TODO: Calculate how far one revolution is in degrees
+        double t;
+        t = (RobotData.encoderTicksPerRevolution * amt) / RobotData.degreesPerRevolution;
+        return (int) Math.round(t);
+    }
+}
--- FtcRobotController/src/main/java/com/astronuts/library/opmodefinal/Tester_1.java
+++ FtcRobotController/src/main/java/com/astronuts/library/opmodefinal/Tester_1.java
@@ -1,17 +1,13 @@
 package com.astronuts.library.opmodefinal;
 
+import com.astronuts.library.Drive;
 import com.astronuts.library.movement.EncoderMotor;
-
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.LightSensor;
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
 
-
-/**
- * Created by Choood and Camus on 11/11/2015.
- */
 public class Tester_1 extends LinearOpMode {
 
     DcMotor LeftTemp;
@@ -20,7 +16,9 @@ public class Tester_1 extends LinearOpMode {
     EncoderMotor MotorLeft;
     EncoderMotor MotorRight;
 
-
+    ColorSensor colorSensor;
+    UltrasonicSensor ultrasonicSensor;
+    LightSensor lightSensor;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -47,6 +45,7 @@ public class Tester_1 extends LinearOpMode {
         MotorLeft.move(1400, 0.5);
 
         waitForNextHardwareCycle();
+
 
 
     }
